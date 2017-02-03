1e727c0aa87e5d094c8d051c4c65a62d174a4111
sourabhsaha94 <sssaha2@ncsu.edu>
2017-02-03 16:57:31 -0500

    Kinematic motion section started

---
+++ GameAIAssignment1/src/KinematicMotion.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class KinematicMotion extends PApplet{
+	
+	public void settings(){
+		size(800,800);
+	}
+	
+	public void setup(){
+		
+	}
+	
+	public void draw(){
+		
+	}
+	
+	public static void main(String argv[]){
+		PApplet.main("KinematicMotion");
+	}
+}
